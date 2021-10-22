e385de6ffa969914b025247f2678dd182161289f
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-10-21 22:18:19 -0500

    Update Finding User Purchases.sql

--- Finding User Purchases.sql
+++ Finding User Purchases.sql
@@ -52,6 +52,8 @@ from    Orders
 group by
         CustomerID
 having  count(OrderID) > 1
+-- if you add up the orders in this table that equals 11
+
 
 
 --testing above query
@@ -66,6 +68,80 @@ group by CustomerID
 
 
 
+SELECT *
+FROM Orders
+where CustomerID in 
+		(select CustomerID,
+				 count(OrderID) as order_count
+		from Orders
+		group by CustomerID
+		having count(OrderID) > 1) 
 
 
+-- this returns 11 records of customers with more than 1 order
+SELECT o.*
+FROM Orders o
+inner join 
+		(select CustomerID,
+				 count(OrderID) as order_count
+		from Orders
+		group by CustomerID
+		having count(OrderID) > 1) m
+on o.CustomerID = m.CustomerID
+
+
+-- adding first purchase date column
+SELECT o.*,
+        min(OrderDate) over (partition by o.CustomerID) as first_purchase
+FROM Orders o
+inner join 
+		(select CustomerID,
+				 count(OrderID) as order_count
+		from Orders
+		group by CustomerID
+		having count(OrderID) > 1) m
+on o.CustomerID = m.CustomerID
+
+
+
+-- create a new column called 'Days_between_purchase'
+SELECT fp.CustomerID,
+	   fp.OrderDate,
+	   fp.first_purchase,
+	   ABS(DATEDIFF(DAY, OrderDate, first_purchase)) AS Days_between_purchase      --use ABS to remove neg signs and get the absolute value
+FROM 
+	(SELECT o.*,
+			min(OrderDate) over (partition by o.CustomerID) as first_purchase
+	 FROM Orders o
+	 inner join 
+			(select CustomerID,
+					 count(OrderID) as order_count
+			from Orders
+			group by CustomerID
+			having count(OrderID) > 1) m
+	 on o.CustomerID = m.CustomerID) fp
+
+--at a glance, there are 3 customers who have made purchases within 7 days of first purcahse
+
+
+
+-- FINAL SOLUTION:
+
+-- lets filter out day 0 ( because 0 would be the first purchase) and days greater than 7 
+SELECT fp.CustomerID,
+	   fp.first_purchase,
+	   fp.OrderDate AS second_purchase,           --reordered columns and renamed this column
+	   ABS(DATEDIFF(DAY, OrderDate, first_purchase)) AS Days_between_purchase      
+FROM 
+	(SELECT o.*,
+			min(OrderDate) over (partition by o.CustomerID) as first_purchase
+	 FROM Orders o
+	 inner join 
+			(select CustomerID,
+					 count(OrderID) as order_count
+			from Orders
+			group by CustomerID
+			having count(OrderID) > 1) m
+	 on o.CustomerID = m.CustomerID) fp
+WHERE ABS(DATEDIFF(DAY, OrderDate, first_purchase)) BETWEEN 1 AND 7;  -- have to use between 1 & 7 to exclude the first purchase
 
