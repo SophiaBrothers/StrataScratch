e8230b883a4512dd400862fce950b86e71a4cf6a
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-10-23 14:40:12 -0500

    Update Best Selling Item.sql

--- Best Selling Item.sql
+++ Best Selling Item.sql
@@ -10,3 +10,72 @@ The best selling item is calculated using the formula (unitprice * quantity). Ou
 with the amount paid.
 
 **/
+
+use MyGuitarShop;
+
+GO
+ 
+
+select * from Orders;  --41 rows
+select * from OrderItems;  --47 rows
+select top 2 * from Products
+
+--approach
+-- need to join orders table and orderItems table on orderID, then join the product table on productId
+
+--columns needed: orderID, ProductID, itemprice, Qty, orderDate, productname
+
+--*NOTE:The item id is not the same as the product id. The item id is just the primary key for the orderitem table
+
+--Also, there is no total invoice paid. Would have to add the 
+
+--first join the tables, using order as the master table
+
+SELECT o.*,
+	   oi.*,
+	   p.ProductName
+FROM Orders o
+INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+LEFT JOIN Products p ON oi.ProductID = p.ProductID;
+
+
+
+--the output of the left join and inner join are the same in this case
+
+
+--CREATE A CTE
+with complete_order as(                            --creating CTE 
+	SELECT o.*,
+		   oi.ProductID,
+		   oi.ItemPrice,
+		   oi.Quantity,
+		   p.ProductName
+	FROM Orders o
+	INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+	LEFT JOIN Products p ON oi.ProductID = p.ProductID);
+
+
+
+-- use a window to find the best selling item for each month
+SELECT ct.ProductID,
+	   datepart('month' from OrderDate ) as Month
+FROM
+	(SELECT o.*,
+			   oi.ProductID,
+			   oi.ItemPrice,
+			   oi.Quantity,
+			   p.ProductName
+		FROM Orders o
+		INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+		LEFT JOIN Products p ON oi.ProductID = p.ProductID
+		) as ct;
+
+
+----------------------------------------------------------------------------------------------
+use MyGuitarShop
+
+SELECT DATEPART('month' from OrderDate) as Month
+FROM Orders;
+
+
+-- still getting an error.
