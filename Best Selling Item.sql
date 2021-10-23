9af87d811c1c8ad0c12d4f8e7c21c4b8b9e77e5b
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-10-22 19:26:39 -0500

    Create Best Selling Item.sql

---
+++ Best Selling Item.sql
@@ -0,0 +1,12 @@
+----------------------------------------  Best Selling Item  ---------------------------------------------
+/**
+
+https://platform.stratascratch.com/coding/10172-best-selling-item?python=
+
+Best Selling Item
+
+Find the best selling item for each month (no need to separate months by year) where the biggest total invoice was paid.
+The best selling item is calculated using the formula (unitprice * quantity). Output the description of the item along
+with the amount paid.
+
+**/
