125c0a0489a618b125780a59b8c047aff57190db
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-10-24 14:19:22 -0500

    Update Best Selling Item.sql

--- Best Selling Item.sql
+++ Best Selling Item.sql
@@ -79,23 +79,121 @@ FROM complete_order;
 
 ----------------------------------------------------------------------------------------------
 
--- use a window to find the best selling item for each month
+-- use a window to rank the items in each month by their total_price for 
+
+with complete_order as(                            
+			SELECT o.*,
+					oi.ProductID,
+					oi.ItemPrice,
+					oi.Quantity,
+					p.ProductName,
+					abs(ItemPrice * Quantity) as total_price,
+		            datename(month,OrderDate ) as Month
+			FROM Orders o
+			INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+			LEFT JOIN Products p ON oi.ProductID = p.ProductID)
+SELECT ProductName,
+	   Month,
+	   total_price,
+	   Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) as rank
+FROM complete_order;
+ 
+
+-- some products have the same ranking within a month
+
+-- add group by to get more accurate results
+
+with complete_order as(                            
+			SELECT o.*,
+					oi.ProductID,
+					oi.ItemPrice,
+					oi.Quantity,
+					p.ProductName,
+					abs(ItemPrice * Quantity) as total_price,
+		            datename(month,OrderDate ) as Month
+			FROM Orders o
+			INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+			LEFT JOIN Products p ON oi.ProductID = p.ProductID)
+SELECT ProductName,
+	   Month,
+	   total_price,
+	   Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) as rank
+FROM complete_order
+GROUP BY ProductName, Month, total_price;
+
+-- rankings now make more sense
+
+
+-- NOW LET'S OUTPUT THE BEST SELLING ITEM FOR EACH MONTH
+
+with complete_order as(                            
+			SELECT o.*,
+					oi.ProductID,
+					oi.ItemPrice,
+					oi.Quantity,
+					p.ProductName,
+					abs(ItemPrice * Quantity) as total_price,
+		            datename(month,OrderDate ) as Month
+			FROM Orders o
+			INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+			LEFT JOIN Products p ON oi.ProductID = p.ProductID)
+SELECT ProductName,
+	   Month,
+	   total_price,
+	   Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) as Ranking
+FROM complete_order
+WHERE Ranking = 1                 -- will get an error (Invalid column name)
+GROUP BY ProductName, Month, total_price;
+
+
+
+-- trying somthing else
+with complete_order as(                            
+			SELECT o.*,
+					oi.ProductID,
+					oi.ItemPrice,
+					oi.Quantity,
+					p.ProductName,
+					abs(ItemPrice * Quantity) as total_price,
+		            datename(month,OrderDate ) as Month
+			FROM Orders o
+			INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+			LEFT JOIN Products p ON oi.ProductID = p.ProductID)
+SELECT ProductName,
+	   Month,
+	   total_price,
+	   Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) as Ranking
+FROM complete_order
+WHERE Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) = 1      -- will also get an error (Windowed functions can only appear in the SELECT or ORDER BY clauses.)
+GROUP BY ProductName, Month, total_price;
+
+
+--see: https://sqltheater.com/blog/cant-use-row-number-where/
+
 
 
 with complete_order as(                            
-		SELECT o.*,
-				oi.ProductID,
-				oi.ItemPrice,
-				oi.Quantity,
-				p.ProductName
-		FROM Orders o
-		INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
-		LEFT JOIN Products p ON oi.ProductID = p.ProductID)
-SELECT OrderID,
-		ProductID,
-		ProductName,
-		ItemPrice,
-		Quantity,
-		abs(ItemPrice * Quantity) as total_price,
-		datename(month,OrderDate ) as Month
+			SELECT o.*,
+					oi.ProductID,
+					oi.ItemPrice,
+					oi.Quantity,
+					p.ProductName,
+					abs(ItemPrice * Quantity) as total_price,
+		            datename(month,OrderDate ) as Month
+			FROM Orders o
+			INNER JOIN OrderItems oi ON o.OrderID = oi.OrderID
+			LEFT JOIN Products p ON oi.ProductID = p.ProductID)
+SELECT ProductName,
+	   Month,
+	   total_price,
+	   CASE
+	   WHEN Rank() OVER (PARTITION BY Month ORDER BY total_price DESC) = 1 then 1 ELSE NULL END as ranking
 FROM complete_order
+GROUP BY ProductName, Month, total_price 
+
+
+--would prefer to omit records that do not have ranking of 1
+
+
+
+-see https://learnsql.com/blog/using-case-data-modifying-statements/
