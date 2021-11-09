cf52a27542a60a9367a0f64e30ac3b79d1ff3234
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-11-09 13:15:09 -0600

    Create Most Profitable.sql

---
+++ Most Profitable.sql
@@ -0,0 +1,19 @@
+----------------------------------- MOST PROFITABLE -------------------------------------------------
+/**
+SEE: https://platform.stratascratch.com/coding/9680-most-profitable-companies?python=
+
+Most Profitable Companies
+
+Find the 3 most profitable companies in the entire world.
+Output the result along with the corresponding company name.
+Sort the result based on profits in descending order.
+
+
+**/
+--TEST USING BIKESDB
+
+use BikesDB
+
+select * from BusinessPartners
+
+select * from SalesOrders
