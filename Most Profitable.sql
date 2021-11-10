d30074b4aff58bf67f1b345279e8165af176cc8b
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-11-10 14:33:28 -0600

    Update Most Profitable.sql

--- Most Profitable.sql
+++ Most Profitable.sql
@@ -12,8 +12,40 @@ Sort the result based on profits in descending order.
 **/
 --TEST USING BIKESDB
 
+
+-- EXPLORING
 use BikesDB
 
-select * from BusinessPartners
+select * from BusinessPartners   --40 rows
 
-select * from SalesOrders
+select distinct COMPANYNAME      --40 rows
+from BusinessPartners
+
+-- columns needed: partnerID, companyname,
+
+select * from SalesOrders         --334 rows
+
+--checking the math
+select 
+	(GROSSAMOUNT - NETAMOUNT) as tax_amt
+from SalesOrders
+
+-- columns needed: partnerID, grossamount
+
+
+-- APPROACH
+--left join BusinessPartners to salesOrder
+--create a subquery
+--rank company using a window function
+
+
+SELECT COMPANYNAME as Company,
+	   sum(GROSSAMOUNT) as Total_Gross,
+	   DENSE_RANK() over(order by sum(GROSSAMOUNT) DESC) as company_rank
+FROM (
+	SELECT s.PARTNER_ID,
+		   s.GROSSAMOUNT,
+		   p.COMPANYNAME
+	FROM SalesOrders s
+	LEFT JOIN BusinessPartners p ON s.PARTNER_ID = p.PARTNER_ID ) as sub
+GROUP BY COMPANYNAME
